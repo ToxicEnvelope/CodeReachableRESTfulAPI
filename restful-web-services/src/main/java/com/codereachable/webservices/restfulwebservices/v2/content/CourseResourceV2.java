1e172207b497bdc36b053994b36abd2b636d5790
ToxicEnvelope <ynhoffman@gmail.com>
2018-02-12 08:55:55 +0200

    adding a course update capability API ; changing the _courseName @Size max validation to 15 chars

--- restful-web-services/src/main/java/com/codereachable/webservices/restfulwebservices/v2/content/CourseResourceV2.java
+++ restful-web-services/src/main/java/com/codereachable/webservices/restfulwebservices/v2/content/CourseResourceV2.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.codereachable.webservices.restfulwebservices.v2.utils.repositories.CourseV2Repository;
 
 @RestController
-@RequestMapping("/v2/courses")
+@RequestMapping("/v2")
 public class CourseResourceV2 {
 	
 	// Fields
@@ -32,7 +33,7 @@ public class CourseResourceV2 {
 	
 	//GET /courses
 	// output -> retrieve all courses
-	@GetMapping("/")
+	@GetMapping("/courses")
 	public List<CourseV2> retrieveAllCourse() {
 		return courseRepository.findAll();
 	}
@@ -40,7 +41,7 @@ public class CourseResourceV2 {
 	//GET /courses/{id}
 	// input -> a course id
 	// output -> return a specific course given an id
-	@GetMapping("/{id}")
+	@GetMapping("/courses/{id}")
 	public Optional<CourseV2> retrieveCourse(@PathVariable String id) {
 		Optional<CourseV2> c = courseRepository.findById(id);
 		if (!c.isPresent()) {
@@ -56,7 +57,7 @@ public class CourseResourceV2 {
 	//DELETE /courses/{id}
 	// input -> a course id
 	// output -> return a specific course given an id
-	@DeleteMapping("/{id}")
+	@DeleteMapping("/courses/{id}")
 	public void deleteCourse(@PathVariable String id) {
 		Optional<CourseV2> c = courseRepository.findById(id);
 		if (!c.isPresent()) {
@@ -72,7 +73,7 @@ public class CourseResourceV2 {
 	//POST
 	// input -> course object
 	// output -> CREATED & return the current URI
-	@PostMapping("/")
+	@PostMapping("/courses")
 	public ResponseEntity<Object> createCourse(@Valid @RequestBody CourseV2 c) {
 		CourseV2 savedCourse = courseRepository.save(c);
 		/*  Build a URI object to represent the CREATED
@@ -85,5 +86,26 @@ public class CourseResourceV2 {
 				.path("/{id}")
 				.buildAndExpand(savedCourse.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	//PUT 
+	// input -> course id
+	// output -> course object
+	@PutMapping("/courses/{id}")
+	public ResponseEntity<Object> updateCourse(@PathVariable String id, @Valid @RequestBody CourseV2 c) {
+		Optional<CourseV2> optionalCourse = Optional.empty();
+		optionalCourse = courseRepository.findById(id);
+		if (!optionalCourse.isPresent()) {
+			throw new CourseV2NotFoundException("id=" + id);
+		}
+		CourseV2 currerntCourse = optionalCourse.get();
+		courseRepository.delete(currerntCourse);
+		CourseV2 savedCourse = courseRepository.save(c);
+		URI uri  = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.buildAndExpand(savedCourse.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+				
+		
 	}
 }
